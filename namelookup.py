90b3c4bcabb46095f91a645b258da654ab87f649
Jim Skon <skonjp@kenyon.edu>
2019-09-14 06:41:06 -0400

    Updated to simplified index

--- namelookup.py
+++ namelookup.py
@@ -11,82 +11,91 @@ cgitb.enable()
 from sortedcontainers import SortedDict
 filePath="/home/class/SoftDev/namedata/"
 
-class NameInfo:
+class nameEntry:
+  """
+  Class to store data for a single name
+  """
+  def __init__(self, line : str):
+    """
+    Takes a single line and parses it into
+    the fields
+    """
+    line=line.strip()
+    data=line.split()
+    self.name=data[0]
+    self.percent=data[1]
+    self.cumulative=data[2]
+    self.rank=data[3]
+
+class NameMap:
   """
   Class to store information about the 
   Statistics of names.
   For each name type, there is a dict
   for each name, there is a list of name %, cumulative %, rank
   """
-  def __init__(self, num):
-    """ 
-    create a NameInfo Structure
-    num is the number of types of names
-    """
-    self.nameData=[]
-    for i in range(num):
-      self.nameData.append([])
-  def addNames(self, nameFile, index):
+  def __init__(self,nameFile : str):
     """
     create a new list of names with data
     given an index within range of 0..num-1
     """
     names = open(filePath+nameFile)
-    nameDict=SortedDict()
+    self.namemap=SortedDict()
     for line in names:
-      line=line.strip()
-      data=line.split()
-      numData=data[1:]
-      nameDict[data[0]]=numData
-    self.nameData[index]=nameDict
+      nameData=nameEntry(line)
+      self.namemap[nameData.name]=nameData
     return
-  def lookup(self,name,index):
+  def lookup(self,name):
     """
-    lookup name in specified index
-    return list of [%,%cum,rank] if in list
+    lookup name in map
+    return nameEntry
     else return none
     """
-    return self.nameData[index].get(name)
+    return self.namemap.get(name)
 
-  def lookup10(self,name,index):
+  def lookup10(self,name):
     """
     lookup name in specified index
-    return list of [%,%cum,rank] if in list
+    return list of <name,[%,%cum,rank]> if in list
     else return none
     """
-    i=self.nameData[index].bisect_right(name)
+    i=self.namemap.bisect_right(name)
     low = max(0,i-5)
-    high = min(len(self.nameData[index]),i+5)
-    result=""
+    high = min(len(self.namemap),i+5)
+    result = []
     for j in range(low,high):
-      result+=self.nameData[index].peekitem(j)[0]+","
-      result+=self.nameData[index].peekitem(j)[1][0]+","
-      result+=self.nameData[index].peekitem(j)[1][2]+","
-    result=result[:-2]  
+      result.append(self.namemap.peekitem(j))
     return result
 
+
+def lookup(name,data):
+  print(data[name])
 
 def print_header():
     print ("""Content-type: text/html\n""")
 
 def main():
-  nameInfo=NameInfo(3)
-  nameInfo.addNames('dist.female.first',0)
-  nameInfo.addNames('dist.male.first',1)
-  nameInfo.addNames('dist.all.last',2)
+  femaleMap=NameMap('dist.female.first')
+  maleMap=NameMap('dist.male.first')
+  lastMap=NameMap('dist.all.last')
 
   form = cgi.FieldStorage()
   if (form.getvalue("name") and form.getvalue("type_select")):
       print_header()
       name=form.getvalue("name").upper()
       ltype=form.getvalue("type_select")
-      index=2
       if ltype=="female":
-        index=0
+        data=femaleMap.lookup10(name)
       elif ltype=="male":
-        index=1
-      data=nameInfo.lookup10(name,index)
-      print(data)
+        data=maleMap.lookup10(name)
+      else:
+        data=lastMap.lookup10(name)
+        result=""
+        for namedata in data:
+          if len(result) > 0:
+            result+=","
+          result+=namedata[1].name+","+namedata[1].percent+","+namedata[1].rank
+        print(result)
   else:
       print("Error in submission")
         
